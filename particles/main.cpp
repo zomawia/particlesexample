b2ac2276e143b6b439a48245de7e868cdf45e7b0
zomawia.sailo <zomawia.sailo@SEA-8E40F1>
2017-02-07 12:30:12 -0800

    part

--- particles/main.cpp
+++ particles/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include "sfwdraw.h"
+#include "maths.h"
+#include "particles.h"
 
 void main() {
 	sfw::initContext();
 
-	while (sfw::stepContext()) {
+	unsigned sprite = sfw::loadTextureMap("../res/particle_sprite.png");
 
+	particle part;
+	part.sprite = sprite;
+	part.pos = vec2{ 400,0 };
+	part.vel = vec2{ 0,20 };
+	part.sDim = vec2{ 8,8 };
+	part.eDim = vec2{ 256, 256 };
+	part.sColor.ui_color = GREEN;
+	part.eColor.ui_color = BLUE;
+	part.lifespan = 12.f;
+	part.lifetime = 0;
+
+	while (sfw::stepContext()) {
+		float dt = sfw::getDeltaTime();
+		
+		part.refresh(dt);
 
 
 	}
---
+++ particles/particles.h
@@ -0,0 +1,27 @@
+#pragma once
+#include"sfwdraw.h"
+#include "maths.h"
+
+struct particle {
+	unsigned sprite;
+	vec2 pos, vel;
+	vec2 sDim, eDim;
+	color sColor, eColor;
+
+	float lifetime, lifespan;
+
+	bool refresh(float dt) {
+		lifetime += dt;
+
+		float alpha = lifetime / lifespan;
+
+		vec2 dim = lerp(sDim, eDim, alpha);
+		color color = lerp(sColor, eColor, alpha);
+
+		pos = pos + vel * dt;
+
+		sfw::drawTexture(sprite, pos.x, pos.y, dim.x, dim.y, 0, true, 0, color.ui_color);
+
+		return alpha < 1;
+	}
+};
